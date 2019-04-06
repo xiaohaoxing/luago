b971e712a71660087e67ee29973e0d87ef83bdef
xiaohaoxing <1009789268@qq.com>
2019-04-07 01:32:59 +0800

    ✅ 完成第 2️⃣ 章头部和函数原型的定义。

---
+++ go/ch02/src/luago/binchunk/binary_chunk.go
@@ -0,0 +1,53 @@
+package binchunk
+
+type binaryChunk struct {
+	header				// 头部
+	sizeUpvalues byte	// 主函数 upvalue 数量
+	mainFunc *Prototype	// 主函数原型
+}
+
+// lua chunk 头信息
+type header struct {
+	signature 		[4]byte	// Magic Number：0x1B4C7561:Esc L u a
+	version  		byte	// 版本号
+	format			byte	// 格式	
+	luacData		[6]byte	// 
+	cintSize		byte	//
+	sizetSize		byte	//
+	instructionSize byte	//
+	luaIntegerSize	byte	//
+	luaNumberSize	byte 	//
+	luacInt			int64	//
+	luacNum			float64	//
+}
+
+// 各个头信息的默认值
+const (
+	LUA_SIGNITURE = "\x1bLua"
+	LUAC_VERSION = 0x53
+	LUAC_FORMAT = 0
+	LUAC_DATA = "\x19\x93\r\n\x1a\n"
+	CINT_SIZE = 4
+	CSIZET_SIZE = 8
+	INSTRUCTION_SIZE = 4
+	LUA_INTEGER_SIZE = 8
+	LUA_NUMBER_SIZE = 8
+	LUAC_INT = 0x5678
+	LUAC_NUM = 370.5
+)
+
+type Prototype struct {
+	Source string
+	LineDefined uint32
+	LastLineDefined uint32
+	NumParams byte
+	IsVararg byte
+	MaxStackSize byte
+	Code []uint32
+	Constants []interface{}
+	Upvalues []Upvalue
+	Protos []*Prototype
+	LineInfo []uint32
+	LocVars []LocVar
+	UpvalueNames []string
+}
