826623d329b702cb22ec12d92b7d03c2904bc155
xiaohaoxing <1009789268@qq.com>
2019-06-12 00:50:54 +0800

    ✅ 完成到 4.3.2。

---
+++ go/ch04/src/luago/api/lua_state.go
@@ -0,0 +1,40 @@
+package api
+
+type LuaType = int
+
+type LuaState interface {
+	GetTop() int
+	AbsIndex(idx int) int
+	CheckStack(n int) bool
+	Pop(n int)
+	Copy(fromIdx, toIdx int)
+	PushValue(idx int)
+	Replace(idx int)
+	Insert(idx int)
+	Remove(idx int)
+	Rotate(idx int)
+	SetTop(idx int)
+
+	TypeName(tp LuaType) string
+	Type(idx int) LuaType
+	IsNone(idx int) bool
+	IsNil(idx int) bool
+	IsNoneOrNil(idx int) bool
+	IsBoolean(idx int) bool
+	IsInteger(idx int) bool
+	IsNumber(idx int) bool
+	IsString(idx int) bool
+	ToBoolean(idx int) bool
+	ToInteger(idx int) int64
+	ToIntegerX(idx int) (int64, bool)
+	ToNumber(idx int) float64
+	ToNumberX(idx int) (float64, bool)
+	ToString(idx int) string
+	ToStringX(idx int) (string, bool)
+
+	PushNil()
+	PushBoolean(b bool)
+	PushInteger(n int64)
+	PushNumber(n float64)
+	PushString(s string)
+}
