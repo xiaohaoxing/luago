c4b076e0304f1e12b03bda9fdadf6c2baf5ee587
xiaohaoxing <1009789268@qq.com>
2019-06-23 18:41:55 +0800

    ✅ 小修改代码。

--- go/ch06/src/luago/vm/instruction.go
+++ go/ch06/src/luago/vm/instruction.go
@@ -2,25 +2,37 @@ package vm
 
 import "luago/api"
 
+const MAXARG_Bx = 1<<18 - 1       // 262143
+const MAXARG_sBx = MAXARG_Bx >> 1 // 131071
+
+/*
+ 31       22       13       5    0
+  +-------+^------+-^-----+-^-----
+  |b=9bits |c=9bits |a=8bits|op=6|
+  +-------+^------+-^-----+-^-----
+  |    bx=18bits    |a=8bits|op=6|
+  +-------+^------+-^-----+-^-----
+  |   sbx=18bits    |a=8bits|op=6|
+  +-------+^------+-^-----+-^-----
+  |    ax=26bits            |op=6|
+  +-------+^------+-^-----+-^-----
+ 31      23      15       7      0
+*/
 type Instruction uint32
 
-// 取值范围
-const MAXARG_Bx = 1<<18 - 1       // 2^18 - 1 = 262143
-const MAXARG_sBx = MAXARG_Bx >> 1 // 262143 / 2 = 131071
-
 func (self Instruction) Opcode() int {
-	return int(self & 0x3f)
+	return int(self & 0x3F)
 }
 
 func (self Instruction) ABC() (a, b, c int) {
-	a = int(self >> 6 & 0xff)
-	b = int(self >> 14 & 0x1ff)
-	c = int(self >> 23 & 0x1ff)
+	a = int(self >> 6 & 0xFF)
+	c = int(self >> 14 & 0x1FF)
+	b = int(self >> 23 & 0x1FF)
 	return
 }
 
 func (self Instruction) ABx() (a, bx int) {
-	a = int(self >> 6 & 0xff)
+	a = int(self >> 6 & 0xFF)
 	bx = int(self >> 14)
 	return
 }
@@ -34,7 +46,6 @@ func (self Instruction) Ax() int {
 	return int(self >> 6)
 }
 
-/** getters **/
 func (self Instruction) OpName() string {
 	return opcodes[self.Opcode()].name
 }
